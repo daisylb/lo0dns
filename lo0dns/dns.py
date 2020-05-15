db5567f7a00004c5f93ada6656082e3c53cbfba1
Leigh Brenecki <l@leigh.net.au>
2020-05-15 21:49:40 +0930

    i am also learning things about dns

--- lo0dns/dns.py
+++ lo0dns/dns.py
@@ -1,9 +1,11 @@
 from .util import ensure_returns_deferred
-from twisted.names.dns import Query, RRHeader, Record_AAAA, AAAA, ALL_RECORDS
+from twisted.names.dns import Query, RRHeader, Record_AAAA, AAAA, ALL_RECORDS, \
+    Record_SOA, SOA
 from twisted.names.error import DomainError
 import typing as t
 from cityhash import CityHash32
 from ipaddress import IPv6Network
+from time import time
 
 NETWORK = IPv6Network('fd7f:1fa7:68ca:202f::/64')
 
@@ -25,21 +27,40 @@ class Resolver:
         return answers, [], []
 
 
+SOA_OBJ = RRHeader(
+    name='lo0.wtf',
+    type=SOA,
+    ttl=60,
+    payload=Record_SOA(
+    mname='lo0-ns1.leigh.party',
+    rname='l.leigh.net.au',
+    serial=int(time() / 10),
+    refresh=60,
+    retry=60,
+    expire=60,
+    minimum=60,
+))
+
 async def get_response(query: Query):
     name = query.name.name
     if name.endswith(b'.'): name = name[:-1]
     if not name.endswith(b'.lo0.wtf'): raise DomainError()
-    if query.type not in (AAAA, ALL_RECORDS): return
-    name = name[:-8]
-    vhost, username = name.rsplit(b'.')
-    vhost_hash = CityHash32(vhost)
-    username_hash = CityHash32(username)
-    ip = NETWORK[(username_hash << 32) + vhost_hash]
-    return [RRHeader(
-                name=query.name.name,
-                type=AAAA,
-                payload=Record_AAAA(address=str(ip).encode('ascii')),
-            )]
+    results = []
+    if query.type in (AAAA, ALL_RECORDS):
+        name = name[:-8]
+        vhost, username = name.rsplit(b'.')
+        vhost_hash = CityHash32(vhost)
+        username_hash = CityHash32(username)
+        ip = NETWORK[(username_hash << 32) + vhost_hash]
+        results.append(RRHeader(
+                    name=query.name.name,
+                    type=AAAA,
+                    ttl=60,
+                    payload=Record_AAAA(address=str(ip).encode('ascii')),
+                ))
+    if query.type in (SOA, ALL_RECORDS):
+        results.append(SOA_OBJ)
+    return results
 
 
 if __name__ == "__main__":
@@ -56,7 +77,7 @@ if __name__ == "__main__":
 
     for addr in argv[1:]:
         protocol = dns.DNSDatagramProtocol(controller=factory)
-        reactor.listenUDP(53, protocol, interface=addr)
-        reactor.listenTCP(53, factory, interface=addr)
+        reactor.listenUDP(10053, protocol, interface=addr)
+        reactor.listenTCP(10053, factory, interface=addr)
 
     reactor.run()
